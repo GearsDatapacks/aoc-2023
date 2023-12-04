21ce0ea5ef59044662bf8712da066ab33ed842a7
GearsDatapacks <surya@seriousinfinitude.com>
2023-12-04 14:34:10 +0000

    Complete day 4, part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,26 +1,31 @@
 pub fn main() {
   let input =  std::fs::read_to_string("day4_inputs.txt").unwrap();
-  let mut sum = 0;
-  for line in input.lines() {
-    let numbers = line.split(":").nth(1).unwrap().trim();
-    let winning_numbers: Vec<u32> = numbers.split(" | ")
-                                    .nth(0).unwrap().split_ascii_whitespace()
-                                    .map(str::parse::<u32>).filter_map(Result::ok).collect();
-    let our_numbers: Vec<u32> = numbers.split(" | ")
-                                .nth(1).unwrap().split_ascii_whitespace()
-                                .map(str::parse::<u32>).filter_map(Result::ok).collect();
-
-    let mut num_winning: u32 = 0;
-    for num in winning_numbers {
-      if our_numbers.contains(&num) {
-        num_winning += 1;
-      }
-    }
-
-    if num_winning > 0 {
-      sum += 2_u32.pow(num_winning-1);
+  let mut card_counts: Vec<u32> = vec![1; input.lines().count()];
+  for (line_number, line) in input.lines().into_iter().enumerate() {
+    let wins = process_scratchcard(line);
+    for i in 1..=wins {
+      card_counts[line_number + i as usize] += card_counts[line_number];
     }
   }
 
-  println!("{}", sum);
+  println!("{}", card_counts.iter().sum::<u32>());
+}
+
+fn process_scratchcard(card: &str) -> u32 {
+  let numbers = card.split(":").nth(1).unwrap().trim();
+  let winning_numbers: Vec<u32> = numbers.split(" | ")
+                                  .nth(0).unwrap().split_ascii_whitespace()
+                                  .map(str::parse::<u32>).filter_map(Result::ok).collect();
+  let our_numbers: Vec<u32> = numbers.split(" | ")
+                              .nth(1).unwrap().split_ascii_whitespace()
+                              .map(str::parse::<u32>).filter_map(Result::ok).collect();
+
+  let mut num_winning: u32 = 0;
+  for num in winning_numbers {
+    if our_numbers.contains(&num) {
+      num_winning += 1;
+    }
+  }
+
+  return num_winning;
 }
